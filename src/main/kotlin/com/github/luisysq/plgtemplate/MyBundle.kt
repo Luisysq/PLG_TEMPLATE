a498c1f1b3066ee8b7347292becbcb7e3fdeea1d
GitHub Action <action@github.com>
2025-06-14 23:32:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/luisysq/plgtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/luisysq/plgtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.luisysq.plgtemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/luisysq/plgtemplate/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/luisysq/plgtemplate/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.luisysq.plgtemplate.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
